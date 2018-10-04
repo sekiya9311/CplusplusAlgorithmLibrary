2b8c2027dc4ea1e01cdb1851423616be054bf8b2
sekiya9311 <sekiya9311@gmail.com>
2018-10-05 00:23:33 +0900

    update include_for_test.hpp

--- include_for_test.hpp
+++ include_for_test.hpp
@@ -57,6 +57,9 @@
 #include "util/xor_shift.cpp"
 #include "util/bit_count.cpp"
 #include "util/day_of_month.cpp"
+#include "util/calc_bracket_pair_rank.cpp"
+#include "util/calc_bracket_rank.cpp"
+#include "util/inside.cpp"
 
 // 諸事情により
 #include "math/miller_labin.cpp"
