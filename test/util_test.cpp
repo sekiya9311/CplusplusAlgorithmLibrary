65b6173438aa7e9e8071222f2fa38f64b9d3870c
sekiya9311 <sekiya9311@gmail.com>
2019-01-21 00:41:56 +0900

    add test

--- test/util_test.cpp
+++ test/util_test.cpp
@@ -9,7 +9,20 @@
 #include "../util/calc_bracket_pair_rank.cpp"
 #include "../util/calc_bracket_rank.cpp"
 #include "../util/inside.cpp"
+#include "../util/mod_count_in_range.cpp"
+
+void mod_count_in_range_test() {
+  auto tmp = mod_count_in_range(0, 2, 3);
+  for (auto e : tmp) {
+    assert(e == 1);
+  }
+  tmp = mod_count_in_range(0, 3, 3);
+  for (int i = 0; i < 3; i++) {
+    if (i == 0) assert(tmp[i] == 2);
+    else assert(tmp[i] == 1);
+  }
+}
 
 void util_test() {
-
+  mod_count_in_range_test();
 }
