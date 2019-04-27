c7971ef3e7bf5e0f8147e7b77770a8f04fe7c792
sekiya9311 <sekiya9311@gmail.com>
2019-04-28 01:23:35 +0900

    Add xor_shift test

--- test/util_test.cpp
+++ test/util_test.cpp
@@ -11,6 +11,21 @@
 #include "../util/inside.cpp"
 #include "../util/mod_count_in_range.cpp"
 
+void xor_shift_test() {
+  assert(xor_shift::min() == std::numeric_limits<xor_shift::result_type>::min());
+  assert(xor_shift::max() == std::numeric_limits<xor_shift::result_type>::max());
+  assert(xor_shift::min() == std::numeric_limits<uint32_t>::min());
+  assert(xor_shift::max() == std::numeric_limits<uint32_t>::max());
+  
+  std::vector<int> vec = {0, 1, 2, 3};
+  std::shuffle(std::begin(vec), end(vec), xrnd);
+  std::set<int> set(std::begin(vec), std::end(vec));
+  assert(set.size() == vec.size());
+  for (auto e : vec) {
+    assert(set.find(e) != std::end(set));
+  }
+}
+
 void mod_count_in_range_test() {
   auto tmp = mod_count_in_range(0, 2, 3);
   for (auto e : tmp) {
@@ -24,5 +39,6 @@ void mod_count_in_range_test() {
 }
 
 void util_test() {
+  xor_shift_test();
   mod_count_in_range_test();
 }
