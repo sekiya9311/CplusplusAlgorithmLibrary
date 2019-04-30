7bdfd87ad9bc4e7fd12e6bb0510cf5b792eb2e6f
sekiya9311 <sekiya9311@gmail.com>
2019-04-30 22:06:35 +0900

    Add sort argument of divisor function

--- math/divisor.cpp
+++ math/divisor.cpp
@@ -1,6 +1,6 @@
 // 約数列挙
 //  (O√n)
-std::vector<long long> divisor(const long long n) {
+std::vector<long long> divisor(const long long n, const bool to_sort) {
     std::vector<long long> ret;
     if (n > 1) for(long long i = 1; i * i <= n; i++) {
         if (n % i == 0) {
@@ -10,5 +10,10 @@ std::vector<long long> divisor(const long long n) {
              }
         }
     }
+
+    if (to_sort) {
+        std::sort(std::begin(ret), std::end(ret));
+    }
+
     return ret;
 }
