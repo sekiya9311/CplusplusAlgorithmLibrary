cda187baddd36f3be32148f6fd99307e324bbf11
sekiya9311 <sekiya9311@gmail.com>
2019-06-17 00:58:24 +0900

    refactor simple binary_indexed_tree

--- data_structure/binary_indexed_tree.cpp
+++ data_structure/binary_indexed_tree.cpp
@@ -13,20 +13,20 @@ public:
     binary_indexed_tree() : N(-1) {}
     binary_indexed_tree(int n) : N(n), bit(n, 0) {}
     // bit[point]にnum追加
-    void add(int point, T num) {
+    void add(int point, T val) {
         point++;
-        while (point - 1 < N) {
-            bit[point - 1] += num;
-            point += point & -point;
+
+        for (int i = point; i - 1 < N; i += i & (-i)) {
+            bit[i - 1] += val;
         }
     }
     // [0, point)
     T sum(int point) {
         T res = 0;
-        while (point > 0) {
-            res += bit[point - 1];
-            point -= point & -point;
+        for (int i = point; i > 0; i -= i & (-i)) {
+            res += bit[i - 1];
         }
+
         return res;
     }
     // [left, right)
